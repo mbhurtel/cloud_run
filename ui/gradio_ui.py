3138ada54112f7125742b96f1006ecfde8ba39e0
Manish <manish.bhurtel09@gmail.com>
2024-07-10 21:55:15 -0400

    integrated flask api and ui

--- ui/gradio_ui.py
+++ ui/gradio_ui.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import requests
 
 import os.path as osp
 import sys
@@ -6,18 +7,49 @@ sys.path.append(osp.abspath(osp.join(osp.dirname(__file__), '..')))
 
 from main import main
 
+BASE_URL = "http://localhost:4000"
+
 OPERATIONS = ["Union", "Intersection", "Difference"]
 
-def validate_input(user_set):
-    return set(user_set.split(','))
+def clean_input(user_set):
+    return list(set(user_set.split(',')))
 
 def get_inputs_and_process(operation, setA, setB):
     operation = operation.lower()
-    setA = validate_input(setA)
-    setB = validate_input(setB)
+    setA = clean_input(setA)
+    setB = clean_input(setB)
 
-    result = main(operation, setA, setB)
-    return str(result)
+    # Here we call the api endpoints (setA)
+    response = requests.post(f"{BASE_URL}/setA", json={"setA": setA})
+
+    if response.status_code == 200:
+        result = response.json()
+        setA = result.get("setA", [])
+    else:
+        return f"Error: {response.status_code} - {response.json().get('message', '')}"
+
+    # Here we call the api endpoints (setB)
+    response = requests.post(f"{BASE_URL}/setB", json={"setB": setB})
+
+    if response.status_code == 200:
+        result = response.json()
+        setB = result.get("setB", [])
+    else:
+        return f"Error: {response.status_code} - {response.json().get('message', '')}"
+
+    # Now we call the performOperation endpoint
+    payload = {
+        "operation": operation,
+        "setA": setA,
+        "setB": setB,
+    }
+    
+    response = requests.post(f"{BASE_URL}/performOperation", json=payload)
+    if response.status_code == 200:
+        result = response.json()
+        return result.get("opResult", "No result returned from API.")
+    else:
+        return f"Error: {response.status_code} - {response.json().get('message', '')}"
     
 def initialize_UI_project():
     inputs = [
